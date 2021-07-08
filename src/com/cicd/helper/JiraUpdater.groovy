284856119c206016f0a7f1e515def61034102fc5
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-08 14:30:30 +0530

    Add attachment func

--- src/com/cicd/helper/JiraUpdater.groovy
+++ src/com/cicd/helper/JiraUpdater.groovy
@@ -19,4 +19,14 @@ class JiraUpdater{
         String body = '{\\"body\\": {\\"type\\": \\"doc\\",\\"version\\": 1,\\"content\\": [{\\"type\\": \\"paragraph\\",\\"content\\": [{\\"text\\": \\"'+text+'\\" ,\\"type\\": \\"text\\"}]}]}}'
         return "curl -g --request POST \"https://mstale-test.atlassian.net/rest/api/latest/issue/"+issue_ID+"/comment\" --header \"Authorization: Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA==\" --header \"Content-Type:application/json\" --data-raw \""+body+"\""
     }
+
+    def sendAttachment(Map args = [issueID: "CICD-10", attachmentLink: "www.my_bdd.com"]) {
+    //     return "curl -s -i -X POST \\
+    //  -H \"Authorization:Basic c2hhbnRhbnVkMzkwQGdtYWlsLmNvbTo2YUpLV1VLTzN0bkR6SUZKNE5BRDdBNDE=\" \\
+    //  -H \"X-Atlassian-Token:no-check\" \\
+    //  -F \"file=@C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/jenkins-pipeline-cucumber-example/branches/feature-test-pipeline-cucumber/builds/2/cucumber-html-reports/report-feature_3809222109.html\" \\
+    //  \"https://mstale-test.atlassian.net/rest/api/latest/issue/\" + issueID +\"/attachments\""
+        String issue_ID = args.issueID.toString()
+        return "curl -s -i -X POST \"https://mstale-test.atlassian.net/rest/api/latest/issue/"+issue_ID+"/attachments\" --header \"Authorization:Basic c2hhbnRhbnVkMzkwQGdtYWlsLmNvbTo2YUpLV1VLTzN0bkR6SUZKNE5BRDdBNDE=\" --F \"file=@C:/creport33.html\""
+    }
 }
