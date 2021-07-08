13d176572e10de537a8d720f628151593cb2caf2
Shantanu391 <53044751+Shantanu391@users.noreply.github.com>
2021-07-08 13:49:42 +0530

    addLink deleted

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -29,19 +29,6 @@ def updateComment(Map args =[text: "www.google.com"]){
     bat(script: "curl -g --request POST \"https://mstale-test.atlassian.net/rest/api/latest/issue/"+issue_ID+"/comment\" --header \"Authorization: Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA==\" --header \"Content-Type:application/json\" --data-raw \""+body+"\"")
 }
 
-def addLink(Map args =[text: http://localhost:8081/job/jenkins-pipeline-cucumber-example/job/feature-test-pipeline-cucumber/2/cucumber-html-reports/overview-features.html]){
-    String issue_ID=getIssueID().toString()
-    if(!issueID.equals("")){
-        echo "IssueId found: $issueID"
-    }
-    else{
-        echo "No issueID found!"
-        return
-    }
-
-    String body ='{\\"object\\":{\\"url\\":\\"'+args.text+'\\",\\"title\\":\\"bdd report\\"}}'
-    bat(script: "curl -g --request POST \"https://mstale-test.atlassian.net/rest/api/latest/issue/"+issue_ID+"/remotelink\" --header \"Authorization: Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA==\" --header \"Content-Type:application/json\" --data-raw \""+body+"")
-}
                        
 def addAssignee(Map args =[text: 60dbed7c285656006a7a6927]){
     String issue_ID=getIssueID().toString()
