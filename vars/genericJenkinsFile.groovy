3dea292d94cf98ffc59fab0a6cce6c9eac304b13
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-09 11:50:54 +0530

    Atachment

--- vars/genericJenkinsFile.groovy
+++ vars/genericJenkinsFile.groovy
@@ -225,7 +225,7 @@ def call(Map args =[buildMode: "mvn"]){
                 script{
                     jiraUtil.update(progressLabel: "Deployed",bddReport: "Success", reportLink:"www.my_new_bdd.com")
                     jiraUtil.updateComment(text: "Build Failed")
-                    //jiraUtil.sendAttachment(attachmentLink: "target/site")
+                    jiraUtil.sendAttachment(attachmentLink: "C:/creport33")
                     jiraUtil.addAssignee()
                 }
                 echo "JIRA: Added BDD test reports"
