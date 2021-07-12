9b2cf786f19ce105033f95714b7e7155aa8ff888
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-12 14:18:24 +0530

    more tweaks'

--- src/com/cicd/helper/xmlParse.groovy
+++ src/com/cicd/helper/xmlParse.groovy
@@ -1,9 +1,9 @@
 package com.cicd.helper
 
-def parse(Map args =[ xmlPath: "C:"]){
+def parseXml(Map args =[ xmlPath: "C:"]){
     String xml_path = args.xmlPath.toString()
 
-    def xmlFile = getClass().getResourceAsStream(xml_path)
+    def xmlFile = getClass().getResourceAsStream("@" + xml_path)
 
     def articles = new XmlParser().parse(xmlFile)
 
