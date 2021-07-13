77cabeec72f49acba5505129eb34ad86741d9476
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-13 13:36:54 +0530

    Direct parsing

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -33,7 +33,7 @@ def xmlToComment(Map args = [path: "C:/"]){
     String xmlPath = args.path.toString()
 
     //def xmlFile = readFile xmlPath
-    def xml = new XmlSlurper().parse(xmPath) 
+    def xml = new XmlSlurper().parse(xmlPath) 
     echo "Here"
 
     //echo xml.result.suites.suite[0].name.text()
