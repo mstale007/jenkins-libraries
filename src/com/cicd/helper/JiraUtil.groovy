dd824f3dbb18c5f3dcacb6750a12843ba753077c
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-13 10:17:49 +0530

    Echo xml

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -36,8 +36,9 @@ def xmlToComment(Map args = [path: "C:/"]){
     def xml = new XmlParser().parseText(xmlFile) 
     echo "Here"
    // for(c in xml.result.suites.suite.cases) {
-        echo xml.result.suites[0].cases[0]
+       // echo xml.result.suites[0].cases[0]
    // }
+    echo xml
     // filename = 'cucumber-trends.json'
     // response=bat(script:"type $filename",returnStdout: true).trim()
     // response=response.substring(response.indexOf("\n")+1).trim()
