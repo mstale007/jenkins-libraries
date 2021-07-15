b3ebc32afbfe58781a96a2c9872cfa72ef9a45d9
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-15 17:02:33 +0530

    path corrected

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -44,26 +44,27 @@ def updateComment(Map args =[text: "www.google.com"]){
 }
 
 @NonCPS
-def getJSON(Map args = [response: ""]){
-    def cfg = new JsonSlurper().parse(args.response.toString())
-    //jsonSlurper=null
+def getJSON(response){
+    def jsonSlurper = new JsonSlurperClassic()
+    def cfg = jsonSlurper.parse(response)
+    jsonSlurper=null
     return cfg
 }
 
 def updateCommentwithBDD(Map args = [filePath: "C:/"]) {
     filename = args.filePath.toString()
 
-    // echo filename
-    // echo args.filePath.toString()
-    // if(isUnix()){
-    //     response=sh(script:"cat " + filename,returnStdout: true).trim()
-    // }
-    // else{
-    //     response=bat(script:"type " + filename,returnStdout: true).trim()
-    //     response=response.substring(response.indexOf("\n")+1).trim()
-    // }
+    echo filename
+    echo args.filePath.toString()
+    if(isUnix()){
+        response=sh(script:"cat " + filename,returnStdout: true).trim()
+    }
+    else{
+        response=bat(script:"type " + filename,returnStdout: true).trim()
+        response=response.substring(response.indexOf("\n")+1).trim()
+    }
 
-    def cucumber_json=getJSON(filename)
+    def cucumber_json=getJSON(response)
 
     String table_seperator=""
     if(isUnix()){
--- vars/genericJenkinsFile.groovy
+++ vars/genericJenkinsFile.groovy
@@ -85,7 +85,7 @@ def call(Map args =[buildMode: "mvn"]){
                                 ]
                             ]
                         script {
-                            jiraUtil.updateCommentwithBDD(filePath: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/builds/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4/cucumber-trends.json")
+                            jiraUtil.updateCommentwithBDD(filePath: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4/cucumber-trends.json")
                             jiraUtil.sendAttachment(attachmentLink: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/builds/${env.BUILD_NUMBER}/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4")
                         }
                     }
