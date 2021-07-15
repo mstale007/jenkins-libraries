37679435bade96cfbcb42156bfeeb9f4a333da88
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-15 16:46:55 +0530

    Echo

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -54,15 +54,17 @@ def getJSON(response){
 def updateCommentwithBDD(Map args = [filePath: "C:/"]) {
     filename = args.filePath.toString()
 
-    // if(isUnix()){
-    //     response=sh(script:"cat " + filename,returnStdout: true).trim()
-    // }
-    // else{
-    //     response=bat(script:"type " + filename,returnStdout: true).trim()
-    //     response=response.substring(response.indexOf("\n")+1).trim()
-    // // }
+    echo filename
+    echo args.filePath.toString()
+    if(isUnix()){
+        response=sh(script:"cat " + filename,returnStdout: true).trim()
+    }
+    else{
+        response=bat(script:"type " + filename,returnStdout: true).trim()
+        response=response.substring(response.indexOf("\n")+1).trim()
+    }
 
-    def cucumber_json=getJSON(filename)
+    def cucumber_json=getJSON(response)
 
     String table_seperator=""
     if(isUnix()){
