d5b7566b4d232bf0b82877359682ece4c093446c
mstale <mstale20@gmail.com>
2021-07-15 23:48:44 +0530

    Test 2

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -134,6 +134,7 @@ def checkForIssueIdRegex(Map args=[message:"",startIndex: 0]){
     while(issueKeyEnd<args.message.length() && args.message[issueKeyEnd].matches("[A-Z]")){
         issueKeyEnd++
     }
+    echo args.message
     echo "issueKeyStart: $issueKeyStart, issueKeyEnd: $issueKeyEnd"
     //If no capital letters found
     if(issueKeyEnd==issueKeyStart || args.message[issueKeyEnd]!="-"){
