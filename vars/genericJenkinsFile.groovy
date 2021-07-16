1613c320291513a542249a61c2eb7f15c7150372
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-16 23:36:20 +0530

    Error removed

--- vars/genericJenkinsFile.groovy
+++ vars/genericJenkinsFile.groovy
@@ -42,7 +42,7 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]) {
                     
                     script {
                         LAST_STAGE = env.STAGE_NAME
-                        error "Prohram failed"
+                        //error "Prohram failed"
                         if(isUnix()) {
                             sh "mvn clean install -DskipTests"
                         }
@@ -137,7 +137,7 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]) {
                 script {
                     String issueID = jiraUtil.getIssueID().toString()
                     if(!issueID.equals("")){
-                        jiraUtil.updateComment(text: "Build #$env.BUILD_NUMBER Successful", issue: issueID)
+                        jiraUtil.updateComment(text: "Build #$env.BUILD_NUMBER: Successful", issue: issueID)
                         jiraUtil.xmlToComment(path: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/builds/${env.BUILD_NUMBER}/junitResult.xml", issue: issueID)                    
                         //jiraUtil.updateCommentwithBDD(filePath: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/cucumber-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4/cucumber-trends.json", issue: issueID)
                         jiraUtil.sendAttachment(attachmentLink: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/builds/${env.BUILD_NUMBER}/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4", issue: issueID)
@@ -156,7 +156,7 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]) {
                         jiraUtil.addAssignee(issue: issueID)
                     }
 
-                    jiraUtil.updateComment(text: "Build #$env.BUILD_NUMBER Failed at stage $LAST_STAGE", issue: issueID)
+                    jiraUtil.updateComment(text: "Build #$env.BUILD_NUMBER: Failed at stage $LAST_STAGE", issue: issueID)
                     if(env.UNIT_TEST_REPORT == true) {
                         jiraUtil.xmlToComment(path: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/builds/${env.BUILD_NUMBER}/junitResult.xml", issue: issueID)                    
                     }
