bd62a56b93d8c19f6fe8dd9f6d6d2a6149a3d793
Shantanu391 <53044751+Shantanu391@users.noreply.github.com>
2021-07-16 15:18:36 +0530

    Update JiraUtil.groovy

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -66,6 +66,7 @@ def getAccountId(){
     }
     else{
         String commitEmail = bat(returnStdout: true, script: "git log -1 --pretty=format:'%%ae'")
+        echo "email: $commitEmail"
         response = bat(returnStdout: true,script:"curl --request GET \"https://mstale-test.atlassian.net/rest/api/latest/user/search?query="+commitEmail+" \" -H \"Authorization:Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA== \"  -H \"Accept: application/json \" -H \"Content-Type: application/json\"").trim()
         response = response.substring(response.indexOf("\n")+1).trim()
     }                  
