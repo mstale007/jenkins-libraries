2127fa65dfb25e9016087e484d3bf51cdc02b920
Shantanu391 <53044751+Shantanu391@users.noreply.github.com>
2021-07-16 15:10:43 +0530

    Update JiraUtil.groovy

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -65,7 +65,7 @@ def getAccountId(){
         response = sh(returnStdout: true,script:"curl --request GET \"https://mstale-test.atlassian.net/rest/api/latest/user/search?query="+commitEmail+" \" -H \"Authorization:Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA==  \"  -H \"Accept: application/json \" -H \"Content-Type: application/json\"")
     }
     else{
-        String commitEmail = bat(returnStdout: true, script: "git log -1 --pretty=format:'%ae'")
+        String commitEmail = bat(returnStdout: true, script: "git log -1 --pretty=format:'%%ae'")
         response = bat(returnStdout: true,script:"curl --request GET \"https://mstale-test.atlassian.net/rest/api/latest/user/search?query="+commitEmail+" \" -H \"Authorization:Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA== \"  -H \"Accept: application/json \" -H \"Content-Type: application/json\"").trim()
         response = response.substring(response.indexOf("\n")+1).trim()
     }                  
