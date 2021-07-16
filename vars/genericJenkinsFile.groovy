71cf5bda5963ba1a5256004121c2f41ddd6e3627
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-16 14:26:09 +0530

    Generalization

--- vars/genericJenkinsFile.groovy
+++ vars/genericJenkinsFile.groovy
@@ -19,8 +19,11 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]){
             stage("Initialize"){
                 steps{
                     echo "Stage: $env.STAGE_NAME"
-                    LAST_STAGE = env.STAGE_NAME
                     echo "Branch name is: $env.BRANCH_NAME"
+
+                    script {
+                        LAST_STAGE = env.STAGE_NAME
+                    }
                 }
                 post{
                     success{
@@ -34,8 +37,17 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]){
             stage("Build"){
                 steps{
                     echo "Stage: $env.STAGE_NAME"
-                    LAST_STAGE = env.STAGE_NAME
-                    bat "mvn clean install -DskipTests"
+                    
+                    script {
+                        LAST_STAGE = env.STAGE_NAME
+
+                        if(isUnix()) {
+                            sh "mvn clean install -DskipTests"
+                        }
+                        else {
+                            bat "mvn clean install -DskipTests"
+                        }
+                    }
                 }
                 post{
                     success{
@@ -49,8 +61,17 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]){
             stage("Unit Tests"){
                 steps{
                     echo "Stage: $env.STAGE_NAME"
-                    LAST_STAGE = env.STAGE_NAME
-                    bat "mvn -Dtest=UnitTests test jacoco:report"
+                    
+                    script {
+                        LAST_STAGE = env.STAGE_NAME
+
+                        if(isUnix()) {
+                            sh "mvn -Dtest=UnitTests test jacoco:report"
+                        }
+                        else {
+                            bat "mvn -Dtest=UnitTests test jacoco:report"
+                        }
+                    }
                 }
                 post{
                     always {
@@ -71,9 +92,10 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]){
             stage('Run on localhost') {
                 steps {
                     echo "Stage: $env.STAGE_NAME"
-                    LAST_STAGE = env.STAGE_NAME
 
                     script {
+                        LAST_STAGE = env.STAGE_NAME
+
                         if(isUnix()) {
                             sh "java -jar target/spring-boot-rest-api-tutorial-0.0.1-SNAPSHOT.jar &"
                         }
@@ -86,54 +108,63 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]){
             stage("BDD Test"){
                 steps{
                     echo "Stage: $env.STAGE_NAME"
-                    LAST_STAGE = env.STAGE_NAME
-                    bat "mvn -Dtest=TestRunner test"
-                }
-                post{
-                    always {
-                        cucumber buildStatus: 'UNSTABLE',
-                            reportTitle: 'My report',
-                            fileIncludePattern: '**/*.json',
-                            trendsLimit: 10,
-                            classifications: [
-                                [
-                                    'key': 'Browser',
-                                    'value': 'Firefox'
-                                ]
-                            ]
-                        script {
-                            jiraUtil.updateCommentwithBDD(filePath: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/cucumber-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4/cucumber-trends.json")
-                            jiraUtil.sendAttachment(attachmentLink: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/builds/${env.BUILD_NUMBER}/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4")
+
+                    script {
+                        LAST_STAGE = env.STAGE_NAME
+
+                        if(isUnix()) {
+                            bshat "mvn -Dtest=TestRunner test"
                         }
-                    }
+                        else {
+                            bat "mvn -Dtest=TestRunner test"
+                        }
+                    } 
                 }
             }
             post{
-                always{
-                    script{
-                        //jiraUtil.update(progressLabel: "Deployed",bddReport: "Success", reportLink:"www.my_new_bdd.com")
-                        //jiraUtil.addAssignee()
-                        echo "Now creating.."
-                        
-                    }
-                    echo "JIRA: Added BDD test reports"
-                }
-                success {
-                    echo "Build success"
-                }
-                failure {
+                always {
+                    cucumber buildStatus: 'UNSTABLE',
+                        reportTitle: 'My report',
+                        fileIncludePattern: '**/*.json',
+                        trendsLimit: 10,
+                        classifications: [
+                            [
+                                'key': 'Browser',
+                                'value': 'Firefox'
+                            ]
+                        ]
                     script {
-                        String issueID = jiraUtil.getIssueID().toString()
-                        if(issueID.equals("")){
-                            issueID = jiraUtil.createIssue()
-                        }
-
-                        jiraUtil.updateComment(text: "Build Failed at stage $LAST_STAGE", issue: issueID)
-                    
+                        jiraUtil.updateCommentwithBDD(filePath: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/cucumber-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4/cucumber-trends.json")
+                        jiraUtil.sendAttachment(attachmentLink: "C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/jobs/springboot-multibranch-pipeline/branches/${env.BRANCH_NAME}/builds/${env.BUILD_NUMBER}/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4")
                     }
                 }
-            //cleanup{} 
             }
+        }
+        post{
+            always{
+                script{
+                    //jiraUtil.update(progressLabel: "Deployed",bddReport: "Success", reportLink:"www.my_new_bdd.com")
+                    //jiraUtil.addAssignee()
+                    echo "Now creating.."
+                    
+                }
+                echo "JIRA: Added BDD test reports"
+            }
+            success {
+                echo "Build success"
+            }
+            failure {
+                script {
+                    String issueID = jiraUtil.getIssueID().toString()
+                    if(issueID.equals("")){
+                        issueID = jiraUtil.createIssue()
+                    }
+
+                    jiraUtil.updateComment(text: "Build Failed at stage $LAST_STAGE", issue: issueID)
+                
+                }
+            }
+            //cleanup{} 
         }
     }
 }
