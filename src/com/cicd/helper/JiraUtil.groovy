c7bff896f491cacbbe3baef7d20b8b897d36975d
mstale <mstale20@gmail.com>
2021-07-16 15:28:05 +0530

    Trying ..

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -102,7 +102,7 @@ def getAccountId(){
     else{
         String commitEmail = bat(returnStdout: true, script: "git log -1 --pretty=format:'%%ae'")
         commitEmail=commitEmail.substring(commitEmail.indexOf("\n")+1).trim()
-        commitEmail=commitEmail[1:-1]
+        commitEmail=commitEmail[1..-1]
         echo "$commitEnail"
         response = bat(returnStdout: true,script:"curl --request GET \"https://mstale-test.atlassian.net/rest/api/latest/user/search?query="+commitEmail+" \" -H \"Authorization:Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA== \"  -H \"Accept: application/json \" -H \"Content-Type: application/json\"").trim()
         response = response.substring(response.indexOf("\n")+1).trim()
