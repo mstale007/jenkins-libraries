c271fcf8b99a21c8961c519b688decd00b76713a
mstale <mstale20@gmail.com>
2021-07-16 15:39:56 +0530

    Added updateCommentwithCommitterMention()

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -103,7 +103,6 @@ def getAccountId(){
         commitEmail=commitEmail.substring(commitEmail.indexOf(">")+1).trim()
         commitEmail=commitEmail.substring(commitEmail.indexOf("\n")+1).trim()
         commitEmail=commitEmail[1..-2]
-        echo "commitEmail : $commitEmail"
         response = bat(returnStdout: true,script:"curl --request GET \"https://mstale-test.atlassian.net/rest/api/latest/user/search?query="+commitEmail+" \" -H \"Authorization:Basic bXN0YWxlMjBAZ21haWwuY29tOkhKbFRSQ1B3YmRHMnhabVBIbnhPQUEyRA== \"  -H \"Accept: application/json \" -H \"Content-Type: application/json\"").trim()
         response = response.substring(response.indexOf("\n")+1).trim()
     }                  
