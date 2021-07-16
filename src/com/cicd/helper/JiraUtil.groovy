d26f98cb44b1d9bdc1f694617dfb7faf027cafdf
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-16 23:28:32 +0530

    not build block'

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -75,7 +75,7 @@ def updateCommentwithBDD(Map args = [filePath: "C:/", issue: ""]) {
     }
     
     comment+=table_seperator
-    updateComment(text: "BDD Test Reports:\\n"+comment, issue: issue_ID)
+    updateComment(text: "BDD Test Report for build #$env.BUILD_NUMBER:\\n"+comment, issue: issue_ID)
 }
 
 @NonCPS
@@ -116,7 +116,7 @@ def xmlToComment(Map args = [path: "C:/", issue: ""]){
     String comment = getXML(path: args.path.toString())
     String issue_ID = args.issue.toString()
 
-    updateComment(text: "Junit Test Reports for Build $env.BUILD_NUMBER:\\n" + comment, issue: issue_ID)
+    updateComment(text: "Junit Test Reports for Build #$env.BUILD_NUMBER:\\n" + comment, issue: issue_ID)
 }
 
 def sendAttachment(Map args = [attachmentLink: "target/site/", issue: ""]) {
