015d619e847e0016005b3805c69fc44f1d5cd0e8
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-20 12:15:39 +0530

    BDD comment

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -138,9 +138,9 @@ def getBDD(Map args = [filePath: "$JENKINS_HOME\\jobs\\${env.PIPELINE_NAME}\\bra
         comment+=table_seperator+"*"+element.key.toString().trim()+"*"+table_seperator
     }
     comment+=table_seperator+"\\n"
-    for(element in cucumber_json){
-        comment+=table_seperator+element.value[-1].toString().trim()
-    }
+    // for(element in cucumber_json){
+    //     comment+=table_seperator+element.value[-1].toString().trim()
+    // }
     comment+=table_seperator
     
     // comment+=table_seperator
