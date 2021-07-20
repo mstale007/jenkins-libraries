b394c30943509c0621995ae39dad1e81bf8fd1f7
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-20 11:36:43 +0530

    BDD comment

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -50,7 +50,7 @@ def updateJirawithSuccess(){
     
     //BDD Reports
     commentBody+="{panel:bgColor=#e3fcef}\\nBDD Test Reports:\\n{panel}\\n"
-    commentBody+=getBDD()
+    //commentBody+=getBDD()
     sendAttachment(attachmentLink: "$env.BUILD_FOLDER_PATH/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4", issue: issueID)
 
     //Build Signature
