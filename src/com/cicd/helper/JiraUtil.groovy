f3491c983071763e668e19df727660e728ab5b16
muskaanshraogi <muskaanshraogi123@gmail.com>
2021-07-23 12:17:31 +0530

    Attachment name

--- src/com/cicd/helper/JiraUtil.groovy
+++ src/com/cicd/helper/JiraUtil.groovy
@@ -195,12 +195,12 @@ def sendAttachment(Map args = [attachmentLink: "target/site/", issue: ""]) {
     String link = args.attachmentLink.toString()
 
     if(isUnix()) {
-        sh(script: "zip " + env.BRANCH_NAME + "-BDD-Report-Build-" + env.BUILD_NUMBER + ".zip " + link + "/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4")
-        sh(script: "curl -s -i -X POST \"" + env.JIRA_BOARD + "/issue/"+issue_ID+"/attachments\" --header \"Authorization:" + env.AUTH_TOKEN + "\" --header \"X-Atlassian-Token:no-check\" --form \"file=@" + env.BRANCH_NAME + "-BDD-Report-Build-" + env.BUILD_NUMBER + ".zip\"")
+        sh(script: "zip Build" + env.BUILD_NUMBER + "-" + env.BRANCH_NAME + "-BDD.zip " + link + "/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4")
+        sh(script: "curl -s -i -X POST \"" + env.JIRA_BOARD + "/issue/"+issue_ID+"/attachments\" --header \"Authorization:" + env.AUTH_TOKEN + "\" --header \"X-Atlassian-Token:no-check\" --form \"file=@Build-" + env.BUILD_NUMBER + "-" + env.BRANCH_NAME + "-BDD.zip\"")
     }
     else {
-        bat(script: "powershell Compress-Archive " + link + "/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4 " + env.BRANCH_NAME + "-BDD-Report-Build-" + env.BUILD_NUMBER + ".zip")
-        bat(script: "curl -s -i -X POST \"" + env.JIRA_BOARD + "/issue/"+issue_ID+"/attachments\" --header \"Authorization:" + env.AUTH_TOKEN + "\" --header \"X-Atlassian-Token:no-check\" --form \"file=@" + env.BRANCH_NAME + "-BDD-Report-Build-" + env.BUILD_NUMBER + ".zip\"")
+        bat(script: "powershell Compress-Archive " + link + "/cucumber-html-reports_fb242bb7-17b2-346f-b0a4-d7a3b25b65b4 Build" + env.BUILD_NUMBER + "-" + env.BRANCH_NAME + "-BDD.zip")
+        bat(script: "curl -s -i -X POST \"" + env.JIRA_BOARD + "/issue/"+issue_ID+"/attachments\" --header \"Authorization:" + env.AUTH_TOKEN + "\" --header \"X-Atlassian-Token:no-check\" --form \"file=@Build" + env.BUILD_NUMBER + "-" + env.BRANCH_NAME + "-BDD.zip\"")
     }
 }
 
