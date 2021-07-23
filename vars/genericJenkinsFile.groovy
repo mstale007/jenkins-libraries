c47be89820ed66034ec900a9f67e64a87b70e0ba
mstale <mstale20@gmail.com>
2021-07-23 12:04:22 +0530

    Removed echo junit

--- vars/genericJenkinsFile.groovy
+++ vars/genericJenkinsFile.groovy
@@ -31,7 +31,6 @@ def call(Map args =[buildMode: "mvn", issueKey: ""]) {
                     echo "Stage: $env.STAGE_NAME"
                     echo "Branch name is: $env.BRANCH_NAME"
                     bat "set"
-                    echo "$junit"
                     
                     script {
                         LAST_STAGE = env.STAGE_NAME
